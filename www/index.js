3cd6e1cb3eb1873d983f41273f73433daff6ad41
Masato Bito <m@btmst.jp>
2020-08-26 22:34:25 +0900

    add key event

--- www/index.js
+++ www/index.js
@@ -3,6 +3,60 @@ import { Chip8 } from "wasm-chip8";
 const pre = document.getElementById("display");
 const chip8 = Chip8.new();
 
+document.body.addEventListener("keydown", event => {
+    switch (event.key) {
+        case 'x':
+            chip8.set_key(0x00);
+            break;
+        case '1':
+            chip8.set_key(0x01);
+            break;
+        case '2':
+            chip8.set_key(0x02);
+            break;
+        case '3':
+            chip8.set_key(0x03);
+            break;
+        case 'q':
+            chip8.set_key(0x04);
+            break;
+        case 'w':
+            chip8.set_key(0x05);
+            break;
+        case 'e':
+            chip8.set_key(0x06);
+            break;
+        case 'a':
+            chip8.set_key(0x07);
+            break;
+        case 's':
+            chip8.set_key(0x08);
+            break;
+        case 'd':
+            chip8.set_key(0x09);
+            break;
+        case 'z':
+            chip8.set_key(0x0A);
+            break;
+        case 'c':
+            chip8.set_key(0x0B);
+            break;
+        case '4':
+            chip8.set_key(0x0C);
+            break;
+        case 'r':
+            chip8.set_key(0x0D);
+            break;
+        case 'f':
+            chip8.set_key(0x0E);
+            break;
+        case 'v':
+            chip8.set_key(0x0F);
+            break;
+    }
+    alert(event.key);
+});
+
 const renderLoop = () => {
     pre.textContent = chip8.render();
     chip8.run();
