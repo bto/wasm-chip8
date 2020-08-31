1f053225f59f86f2e875897d1304d59266178fbb
Masato Bito <m@btmst.jp>
2020-08-31 16:29:56 +0900

    add write-file-webpack-plugin move to static directory

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,8 +1,10 @@
 'use strict'
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: process.env.NODE_ENV || "development",
   entry: {
     app: './src/main.js',
   },
@@ -10,10 +12,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bootstrap.js',
   },
-  mode: 'development',
   plugins: [
     new CopyWebpackPlugin({
-      patterns: ['index.html'],
+      patterns: [{
+        from: 'static',
+        to: '.',
+      }],
     }),
+    new WriteFilePlugin(),
   ],
 };
