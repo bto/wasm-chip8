97e77d9370f4b5255de262dd2838ba9c3f0fb453
Masato Bito <m@btmst.jp>
2020-09-05 01:13:12 +0900

    add sample program

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,22 @@
-const wasm = import('./wasm/chip8.js');
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {SubComponent} from './sub-component';
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>Hello React!</h1>
+          <SubComponent name="My Counter for TypeScript"/>
+        </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#app'));
+
+
+const wasm = import('./wasm/chip8');
 wasm.then(mod => {
   mod.greet('foo');
 });
---
+++ web/src/sub-component.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+// Propsの型定義
+interface IProps {
+  name: string;
+}
+
+interface IState {
+  count: number;
+}
+
+export class SubComponent extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleClick() {
+    console.log('クリックされました');
+
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  render() {
+
+    return (
+        <div>
+          <h2>{this.props.name}</h2>
+          <div>{this.state.count}</div>
+          <button onClick={this.handleClick.bind(this)}>Add +1</button>
+        </div>
+    );
+  }
+}
