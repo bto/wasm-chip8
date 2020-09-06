7a31fddca9628aa7d852c570cea4019be7a9f9d4
Masato Bito <m@btmst.jp>
2020-09-06 21:36:33 +0900

    fix by lint

--- web/src/sub-component.tsx
+++ web/src/sub-component.tsx
@@ -1,38 +1,37 @@
-import * as React from 'react';
+import * as React from "react";
 
 // Propsの型定義
 interface IProps {
-  name: string;
+    name: string;
 }
 
 interface IState {
-  count: number;
+    count: number;
 }
 
 export class SubComponent extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-  }
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+    }
 
-  handleClick() {
-    console.log('クリックされました');
+    handleClick(): undefined {
+        console.log("クリックされました");
 
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
+        this.setState({
+            count: this.state.count + 1,
+        });
+    }
 
-  render() {
-
-    return (
-        <div>
-          <h2>{this.props.name}</h2>
-          <div>{this.state.count}</div>
-          <button onClick={this.handleClick.bind(this)}>Add +1</button>
-        </div>
-    );
-  }
+    render(): React.ReactNode {
+        return (
+            <div>
+                <h2>{this.props.name}</h2>
+                <div>{this.state.count}</div>
+                <button onClick={this.handleClick.bind(this)}>Add +1</button>
+            </div>
+        );
+    }
 }
