f20f922373f762a2b48b9fad1711cb3b66d86e24
Masato Bito <m@btmst.jp>
2020-09-06 21:29:42 +0900

    change setting for both javascript and typescript

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,16 +1,34 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 2018,
     sourceType: 'module',
-    project: './tsconfig.json',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  overrides: [{
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+    ],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+      'prettier/@typescript-eslint',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+    plugins: [
+      '@typescript-eslint',
+    ],
+  }],
 };
