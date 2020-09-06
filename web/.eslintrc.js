c27e0245aba3518646402491861fbe749ab7d800
Masato Bito <m@btmst.jp>
2020-09-06 22:00:29 +0900

    fix by lint

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,34 +3,27 @@ module.exports = {
     browser: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  overrides: [{
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-      'plugin:prettier/recommended',
-      'prettier/@typescript-eslint',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      sourceType: 'module',
-      project: './tsconfig.json',
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      plugins: ["@typescript-eslint", "react"],
     },
-    plugins: [
-      '@typescript-eslint',
-      'react',
-    ],
-  }],
+  ],
 };
