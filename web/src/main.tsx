6d23862db0cb74337ed519ca1185178774495ed3
Masato Bito <m@btmst.jp>
2020-09-06 21:52:35 +0900

    fix by lint

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,22 +1,21 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import {SubComponent} from './sub-component';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { SubComponent } from "./sub-component";
 
 class App extends React.Component {
-  render() {
-    return (
-        <div>
-          <h1>Hello React!</h1>
-          <SubComponent name="My Counter for TypeScript"/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+                <SubComponent name="My Counter for TypeScript" />
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector("#app"));
 
-
-const wasm = import('./wasm/chip8');
-wasm.then(mod => {
-  mod.greet('foo');
+const wasm = import("./wasm/chip8");
+wasm.then((mod) => {
+    mod.greet("foo");
 });
