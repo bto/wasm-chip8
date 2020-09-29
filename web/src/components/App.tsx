25db6e16fd1e1de394c79d285b06ff5b843969c2
Masato Bito <m@btmst.jp>
2020-09-29 22:53:47 +0900

    change name

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import * as ReactRedux from "react-redux";
 import store from "../store";
 
-const App: React.FC = () => (
+const component: React.FC = () => (
     <ReactRedux.Provider store={store}>
         <div>App</div>
     </ReactRedux.Provider>
 );
 
-export default App;
+export default component;
