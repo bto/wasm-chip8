2c103ff3a79b0e1914390f02b01f18b960a52fd2
Masato Bito <m@btmst.jp>
2020-10-02 02:41:50 +0900

    add custom hook

--- web/src/components/Message.tsx
+++ web/src/components/Message.tsx
@@ -1,27 +1,14 @@
 import * as React from "react";
-import * as ReactRedux from "react-redux";
-import { actions, State } from "../store";
+import useMessage from "../hooks/useMessage";
 
 const component: React.FC = () => {
-    const [message, setMessage] = React.useState("");
-    const messageState = ReactRedux.useSelector(
-        (state: State) => state.message.message
-    );
-    const dispatch = ReactRedux.useDispatch();
-
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setMessage(event.target.value);
-    };
-
-    const onClick = (): void => {
-        dispatch(actions.message.send(message));
-    };
+    const [message, change, send] = useMessage();
 
     return (
         <div>
-            Message {messageState}
-            <input type="text" value={message} onChange={onChange} />
-            <button onClick={onClick} />
+            Message {message}
+            <input type="text" onChange={change} />
+            <button onClick={send} />
         </div>
     );
 };
