c1db87c1df612c90417b527c4d5e0a91bb1657d9
Masato Bito <m@btmst.jp>
2020-10-10 03:30:33 +0900

    remove Message

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,17 +1,19 @@
 import * as Redux from "redux";
-import messageActions from "./message/actions";
-import messageReducer from "./message/reducer";
+import * as register from "./register";
+import * as vram from "./vram";
 
 export const reducer = Redux.combineReducers({
-    message: messageReducer,
+    register: register.reducer,
+    vram: vram.reducer,
 });
+
+export const actions = {
+    register: register.actions,
+    vram: vram.actions,
+};
 
 export type State = ReturnType<typeof reducer>;
 
-export const actions = {
-    message: messageActions,
-};
-
-const store: Redux.Store<State> = Redux.createStore(reducer);
+export const store: Redux.Store<State> = Redux.createStore(reducer);
 
 export default store;
--- web/src/store/message/actions.ts
+++
@@ -1,9 +0,0 @@
-import actionCreatorFactory from "typescript-fsa";
-
-const actionCreator = actionCreatorFactory("message");
-
-export const actions = {
-    send: actionCreator<string>("SEND"),
-};
-
-export default actions;
