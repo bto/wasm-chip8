c95b15c645078b6c9bc248e84a2f1efba22c7b35
Masato Bito <m@btmst.jp>
2020-10-11 16:31:08 +0900

    not to use div

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -4,12 +4,12 @@ import Ram from "../containers/Ram";
 import Register from "../containers/Register";
 
 const component: React.FC = () => (
-    <div>
+    <>
         <h1>CHIP-8 emulator</h1>
         <Display />
         <Register />
         <Ram />
-    </div>
+    </>
 );
 
 export default component;
