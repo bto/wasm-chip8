fcbcfcb6ae88c0d99ebd1ad037c3af7f2cc10d8b
Masato Bito <m@btmst.jp>
2020-10-16 18:23:07 +0900

    add Header component

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./style.scss";
+import Header from "./Header";
 import Display from "../containers/Display";
 import Ram from "../containers/Ram";
 import Register from "../containers/Register";
@@ -8,7 +9,7 @@ const component: React.FC = () => (
     <div className="c-app">
         <div className="c-sidebar">Sidebar</div>
         <div className="c-wrapper">
-            <header className="c-header">CHIP-8 emulator</header>
+            <Header />
             <div className="c-body">
                 <main className="c-main">
                     <div className="col-lg-6 col-sm-12">
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as CoreUI from "@coreui/react";
+
+const component: React.FC = () => (
+    <CoreUI.CHeader>CHIP-8 emulator</CoreUI.CHeader>
+);
+
+export default component;
