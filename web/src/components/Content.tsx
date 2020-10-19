d01114def9042e5b2a86377f67c52c05a992b444
Masato Bito <m@btmst.jp>
2020-10-19 17:03:37 +0900

    fix design

--- web/src/components/Content.tsx
+++ web/src/components/Content.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
+import { CContainer, CRow, CCol } from "@coreui/react";
 import { Display, Ram, Register } from "../containers";
 
 const component: React.FC = () => (
-    <div className="container-fluid">
-        <Display />
-        <Ram />
-        <Register />
-    </div>
+    <CContainer fluid={true}>
+        <CRow>
+            <CCol lg="5" className="mx-auto">
+                <Display />
+            </CCol>
+            <CCol md="4">
+                <Ram />
+            </CCol>
+            <CCol sm="3">
+                <Register />
+            </CCol>
+        </CRow>
+    </CContainer>
 );
 
 export default component;
