bb4f3cb2040b0d1bde96fe3f62b3686635f23c67
Masato Bito <m@btmst.jp>
2020-10-19 17:11:37 +0900

    fix text data

--- web/src/components/Ram.tsx
+++ web/src/components/Ram.tsx
@@ -8,15 +8,25 @@ type Props = {
 const component: React.FC<Props> = (props: Props) => (
     <div>
         {props.pc + 0}: {props.ram[props.pc + 0]}
+        <br />
         {props.pc + 1}: {props.ram[props.pc + 1]}
+        <br />
         {props.pc + 2}: {props.ram[props.pc + 2]}
+        <br />
         {props.pc + 3}: {props.ram[props.pc + 3]}
+        <br />
         {props.pc + 4}: {props.ram[props.pc + 4]}
+        <br />
         {props.pc + 5}: {props.ram[props.pc + 5]}
+        <br />
         {props.pc + 6}: {props.ram[props.pc + 6]}
+        <br />
         {props.pc + 7}: {props.ram[props.pc + 7]}
+        <br />
         {props.pc + 8}: {props.ram[props.pc + 8]}
+        <br />
         {props.pc + 9}: {props.ram[props.pc + 9]}
+        <br />
     </div>
 );
 
