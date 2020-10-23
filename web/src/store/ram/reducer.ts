44fe5862d257e635181e2964c5e11a184173ffc9
Masato Bito <m@btmst.jp>
2020-10-23 16:01:09 +0900

    fix for strict mode

--- web/src/store/ram/reducer.ts
+++ web/src/store/ram/reducer.ts
@@ -1,7 +1,7 @@
 import { reducerWithInitialState } from "typescript-fsa-reducers";
 import actions from "./actions";
 
-export const reducer = reducerWithInitialState([]).case(
+export const reducer = reducerWithInitialState([] as number[]).case(
     actions.set,
     (_, ram) => ram
 );
