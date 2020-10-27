a83a9a8cbf13b8132f208bafe84057b658f4f2de
Masato Bito <m@btmst.jp>
2020-10-27 14:49:33 +0900

    use for loop

--- web/src/components/Ram.tsx
+++ web/src/components/Ram.tsx
@@ -1,29 +1,14 @@
 import * as React from "react";
 import { Props } from "../containers/Ram";
 
-const component: React.FC<Props> = ({ mod, pc, ram }: Props) => (
-    <div>
-        {mod.toAddr(pc + 0)}: {ram[pc + 0]}
-        <br />
-        {mod.toAddr(pc + 1)}: {ram[pc + 1]}
-        <br />
-        {mod.toAddr(pc + 2)}: {ram[pc + 2]}
-        <br />
-        {mod.toAddr(pc + 3)}: {ram[pc + 3]}
-        <br />
-        {mod.toAddr(pc + 4)}: {ram[pc + 4]}
-        <br />
-        {mod.toAddr(pc + 5)}: {ram[pc + 5]}
-        <br />
-        {mod.toAddr(pc + 6)}: {ram[pc + 6]}
-        <br />
-        {mod.toAddr(pc + 7)}: {ram[pc + 7]}
-        <br />
-        {mod.toAddr(pc + 8)}: {ram[pc + 8]}
-        <br />
-        {mod.toAddr(pc + 9)}: {ram[pc + 9]}
-        <br />
-    </div>
-);
+const component: React.FC<Props> = ({ mod, pc, ram }: Props) => {
+    const items = [];
+    for (let i = 0; i <= 20; i++) {
+        items.push(`${mod.toAddr(pc + i)}: ${ram[pc + i]}`);
+        items.push(<br />);
+    }
+
+    return <div>{items}</div>;
+};
 
 export default component;
