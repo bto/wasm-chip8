003cffcb68e513739ea43bf90e9e48e8ea643e7b
Masato Bito <m@btmst.jp>
2020-10-27 23:34:25 +0900

    use jsx

--- web/src/components/Ram.tsx
+++ web/src/components/Ram.tsx
@@ -5,14 +5,19 @@ import EmuUtil from "../EmuUtil";
 const component: React.FC<Props> = ({ pc, ram }: Props) => {
     const util = new EmuUtil();
 
-    const items = [];
-    for (let i = 0; i <= 20; i++) {
-        const addr = pc + i * 2;
-        items.push(`0x${util.toHex(addr, 3)}: ${util.decode(ram, addr)}`);
-        items.push(<br />);
-    }
-
-    return <div>{items}</div>;
+    return (
+        <div>
+            {ram.slice(pc, pc + 40).map((_, i) => {
+                const addr = pc + i;
+                if (addr % 2 == 1) return;
+                return (
+                    <div key={addr}>
+                        0x{util.toHex(addr, 3)}: {util.decode(ram, addr)}
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default component;
