bf46dd95ee3c6cf6deebd17c22666d5d971dfd72
Masato Bito <m@btmst.jp>
2020-10-27 22:55:44 +0900

    change registers

--- web/src/components/Register.tsx
+++ web/src/components/Register.tsx
@@ -1,28 +1,27 @@
 import * as React from "react";
 import { Props } from "../containers/Register";
+import EmuUtil from "../EmuUtil";
 
-const component: React.FC<Props> = ({ i, sp, pc, v }: Props) => (
-    <div>
-        <div>v0: {v[0x0]}</div>
-        <div>v1: {v[0x1]}</div>
-        <div>v2: {v[0x2]}</div>
-        <div>v3: {v[0x3]}</div>
-        <div>v4: {v[0x4]}</div>
-        <div>v5: {v[0x5]}</div>
-        <div>v6: {v[0x6]}</div>
-        <div>v7: {v[0x7]}</div>
-        <div>v8: {v[0x8]}</div>
-        <div>v9: {v[0x9]}</div>
-        <div>va: {v[0xa]}</div>
-        <div>vb: {v[0xb]}</div>
-        <div>vc: {v[0xc]}</div>
-        <div>vd: {v[0xd]}</div>
-        <div>ve: {v[0xe]}</div>
-        <div>vf: {v[0xf]}</div>
-        <div>i: {i}</div>
-        <div>sp: {sp}</div>
-        <div>pc: {pc}</div>
-    </div>
-);
+const component: React.FC<Props> = ({ i, sp, pc, v }: Props) => {
+    const util = new EmuUtil();
+
+    const vregisters = [];
+    for (let i = 0; i <= 0xf; i++) {
+        vregisters.push(
+            <div>
+                V{util.toHex(i)}: 0x{util.toHex(v[i], 2)}
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            {vregisters}
+            <div>I: 0x{util.toHex(i, 3)}</div>
+            <div>SP: 0x{util.toHex(sp, 3)}</div>
+            <div>PC: 0x{util.toHex(pc, 3)}</div>
+        </div>
+    );
+};
 
 export default component;
