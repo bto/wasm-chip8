86e6ad0293c6db262a552bb06b1eff14d364e493
Masato Bito <m@btmst.jp>
2020-10-27 23:07:38 +0900

    improve jsx

--- web/src/components/Register.tsx
+++ web/src/components/Register.tsx
@@ -5,18 +5,13 @@ import EmuUtil from "../EmuUtil";
 const component: React.FC<Props> = ({ i, sp, pc, v }: Props) => {
     const util = new EmuUtil();
 
-    const vregisters = [];
-    for (let i = 0; i <= 0xf; i++) {
-        vregisters.push(
-            <div>
-                V{util.toHex(i)}: 0x{util.toHex(v[i], 2)}
-            </div>
-        );
-    }
-
     return (
         <div>
-            {vregisters}
+            {v.map((v, i) => (
+                <div key={i}>
+                    V{util.toHex(i)}: 0x{util.toHex(v, 2)}
+                </div>
+            ))}
             <div>I: 0x{util.toHex(i, 3)}</div>
             <div>SP: 0x{util.toHex(sp, 3)}</div>
             <div>PC: 0x{util.toHex(pc, 3)}</div>
